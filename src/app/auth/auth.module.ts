4e4f5cfed906d802f4f5e233b6a484646fc1c7d3
Matheus Botelho <matheusbcdev@gmail.com>
2023-04-15 16:59:50 -0300

    feat: pagina de login

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './pages/login/login.component';
 import { AuthRoutingModule } from './auth.routing';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   declarations: [LoginComponent],
-  imports: [CommonModule, AuthRoutingModule],
+  imports: [CommonModule, AuthRoutingModule, MatButtonModule, MatInputModule],
 })
 export class AuthModule {}
