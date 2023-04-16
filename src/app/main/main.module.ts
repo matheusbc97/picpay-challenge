19c73a49adf428731b43b8cf4dcf688b5f5e845a
Matheus Botelho <matheusbcdev@gmail.com>
2023-04-16 15:18:29 -0300

    feat: criação de componente header

--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -4,9 +4,11 @@ import { CommonModule } from '@angular/common';
 import { MainRoutingModule } from './main-routing.module';
 import { MainComponent } from './root/main.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
-  declarations: [MainComponent, DashboardComponent],
-  imports: [CommonModule, MainRoutingModule],
+  declarations: [MainComponent, DashboardComponent, HeaderComponent],
+  imports: [CommonModule, MainRoutingModule, SharedModule],
 })
 export class MainModule {}
