40b74a0a1077f2f0362782621b09ed3a2cc6d3a7
Matheus Botelho <matheusbcdev@gmail.com>
2023-04-16 17:22:47 -0300

    fix: icon button click

--- src/app/shared/components/icon-button/icon-button.component.ts
+++ src/app/shared/components/icon-button/icon-button.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-icon-button',
@@ -13,4 +13,10 @@ export class IconButtonComponent {
   @Input() buttonAreaLabel = '';
 
   @Input() buttonAreaPressed: boolean | undefined;
+
+  @Output() public buttonClick: EventEmitter<MouseEvent> = new EventEmitter();
+
+  public onClick(event: MouseEvent): void {
+    this.buttonClick.emit(event);
+  }
 }
