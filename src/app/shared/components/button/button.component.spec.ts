6d88eaed7ac104bdd0ab236907659ec7d32f05f3
Matheus Botelho <matheusbcdev@gmail.com>
2023-04-19 20:42:03 -0300

    test: testes do ButtonComponent

--- src/app/shared/components/button/button.component.spec.ts
+++ src/app/shared/components/button/button.component.spec.ts
@@ -1,6 +1,13 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ButtonComponent } from './button.component';
+import { Component } from '@angular/core';
+import { By } from '@angular/platform-browser';
+
+@Component({
+  template: '<app-button>Salvar</app-button>',
+})
+class MockButtonWithTextComponent {}
 
 describe('ButtonComponent', () => {
   let component: ButtonComponent;
@@ -8,9 +15,8 @@ describe('ButtonComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ButtonComponent ]
-    })
-    .compileComponents();
+      declarations: [ButtonComponent, MockButtonWithTextComponent],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(ButtonComponent);
     component = fixture.componentInstance;
@@ -19,5 +25,28 @@ describe('ButtonComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should emit an event when clicked', () => {
+    const spy = spyOn(component.buttonClick, 'emit');
+    const button = fixture.nativeElement.querySelector('button');
+    button.click();
+    expect(spy).toHaveBeenCalled();
+  });
+
+  it('should have the correct type', () => {
+    component.type = 'submit';
+    fixture.detectChanges();
+    const button = fixture.nativeElement.querySelector('button');
+    expect(button.getAttribute('type')).toBe('submit');
+  });
+
+  it('should have the correct text content', () => {
+    const testFixture = TestBed.createComponent(MockButtonWithTextComponent);
+
+    const button = testFixture.debugElement.query(By.css('button'));
+
+    const buttonText = button.nativeElement.textContent.trim();
+    expect(buttonText).toEqual('Salvar');
   });
 });
