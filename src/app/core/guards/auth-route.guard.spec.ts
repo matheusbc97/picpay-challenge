30a749715264d46d2238925118bcfc07d1cecdd3
Matheus Botelho <matheusbcdev@gmail.com>
2023-04-19 18:11:49 -0300

    test: testes do AuthRouteGuard

--- src/app/core/guards/auth-route.guard.spec.ts
+++ src/app/core/guards/auth-route.guard.spec.ts
@@ -1,16 +1,52 @@
-import { TestBed } from '@angular/core/testing';
-
+import { AuthUserService } from '../services/auth-user.service';
+import { Router } from '@angular/router';
 import { AuthRouteGuard } from './auth-route.guard';
 
 describe('AuthRouteGuard', () => {
-  let guard: AuthRouteGuard;
+  let router: Router;
+  let authUserService: AuthUserService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    guard = TestBed.inject(AuthRouteGuard);
+    router = jasmine.createSpyObj('router', ['navigate']);
+    authUserService = jasmine.createSpyObj('authUserService', [
+      'getUserName',
+      'loadUserFromLocalStorage',
+    ]);
   });
 
   it('should be created', () => {
+    const guard = new AuthRouteGuard(router, authUserService);
     expect(guard).toBeTruthy();
+  });
+
+  it('should return true if user is not logged in', () => {
+    const guard = new AuthRouteGuard(router, authUserService);
+    const isUserLoggedIn = guard.canActivate();
+
+    expect(isUserLoggedIn).toBeTruthy();
+  });
+
+  it('should return false if user is logged in', () => {
+    const guard = new AuthRouteGuard(router, authUserService);
+    (authUserService.getUserName as jasmine.Spy).and.returnValue('User');
+    (authUserService.loadUserFromLocalStorage as jasmine.Spy).and.returnValue(
+      false
+    );
+
+    const isUserLoggedIn = guard.canActivate();
+
+    expect(isUserLoggedIn).toBeFalsy();
+  });
+
+  it('should redirect user to main if user is logged in', () => {
+    const guard = new AuthRouteGuard(router, authUserService);
+    (authUserService.getUserName as jasmine.Spy).and.returnValue('User');
+    (authUserService.loadUserFromLocalStorage as jasmine.Spy).and.returnValue(
+      false
+    );
+
+    guard.canActivate();
+
+    expect(router.navigate).toHaveBeenCalledWith(['/main']);
   });
 });
