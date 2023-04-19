995c80daf1286d7368f07f953372bc1e44837e81
Matheus Botelho <matheusbcdev@gmail.com>
2023-04-19 17:41:38 -0300

    test: testes ToastService

--- src/app/core/services/toast.service.spec.ts
+++ src/app/core/services/toast.service.spec.ts
@@ -1,16 +1,42 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed, fakeAsync, tick } from '@angular/core/testing';
 
 import { ToastService } from './toast.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 describe('ToastService', () => {
+  const matSnackBarMock = {
+    open: jasmine.createSpy('open'),
+    dismiss: jasmine.createSpy('dismiss'),
+  };
+
   let service: ToastService;
+  let snackBar: MatSnackBar;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        {
+          provide: MatSnackBar,
+          useValue: matSnackBarMock,
+        },
+      ],
+    });
     service = TestBed.inject(ToastService);
+    snackBar = TestBed.inject(MatSnackBar);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should call snackBar.open', () => {
+    service.open('message');
+    expect(snackBar.open).toHaveBeenCalledTimes(1);
+  });
+
+  it('should call snackBar.dismiss', fakeAsync(() => {
+    service.open('message');
+    tick(4000);
+    expect(snackBar.dismiss).toHaveBeenCalledTimes(1);
+  }));
 });
