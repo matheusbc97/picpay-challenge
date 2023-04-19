eb43720373126f12bacc1a40dba79153408822ec
Matheus Botelho <matheusbcdev@gmail.com>
2023-04-19 20:49:52 -0300

    test: testes do MainComponent

--- src/app/main/root/main.component.spec.ts
+++ src/app/main/root/main.component.spec.ts
@@ -1,6 +1,14 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MainComponent } from './main.component';
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  template: '',
+  selector: 'app-header',
+})
+class MockHeaderComponent {}
 
 describe('MainComponent', () => {
   let component: MainComponent;
@@ -8,7 +16,8 @@ describe('MainComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [MainComponent],
+      declarations: [MainComponent, MockHeaderComponent],
+      imports: [RouterModule],
     }).compileComponents();
 
     fixture = TestBed.createComponent(MainComponent);
