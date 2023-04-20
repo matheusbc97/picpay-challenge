a271f125ec1bd300d1a4a09a10afd180d7313e37
Matheus Botelho <matheusbcdev@gmail.com>
2023-04-19 22:13:52 -0300

    test: testes do GetPaymentsService

--- src/app/main/pages/dashboard/services/get-payments.service.spec.ts
+++ src/app/main/pages/dashboard/services/get-payments.service.spec.ts
@@ -1,16 +1,55 @@
-import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { TestBed, fakeAsync, tick } from '@angular/core/testing';
+import { of } from 'rxjs';
+import { Payment } from 'src/app/shared/models/payment.model';
 
 import { GetPaymentsService } from './get-payments.service';
+import { PaymentsService } from 'src/app/core/services/payments.service';
+import { paymentsMock } from '../mocks/payments.mock';
 
 describe('GetPaymentsService', () => {
   let service: GetPaymentsService;
+  let paymentsService: PaymentsService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [PaymentsService],
+    });
     service = TestBed.inject(GetPaymentsService);
+    paymentsService = TestBed.inject(PaymentsService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should get payments', fakeAsync(() => {
+    const payments: Payment[] = [paymentsMock[0]];
+    spyOn(paymentsService, 'get').and.returnValue(
+      of({ items: payments, totalPage: 1 })
+    );
+
+    let result: any;
+
+    service.get().subscribe((response) => {
+      result = response;
+    });
+
+    service.reload();
+
+    tick();
+
+    expect(result).toEqual(payments);
+  }));
+
+  it('isLoading should be true', () => {
+    service.get();
+
+    expect(service.isLoading).toBeTrue();
+  });
+
+  it('isLoading should be false', () => {
+    expect(service.isLoading).toBeFalse();
   });
 });
