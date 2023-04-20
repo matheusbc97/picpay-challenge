c78be810aa1dd22559d4b52e62b81679da94af9f
Matheus Botelho <matheusbcdev@gmail.com>
2023-04-19 21:40:03 -0300

    test: header component test

--- src/app/main/components/header/header.component.spec.ts
+++ src/app/main/components/header/header.component.spec.ts
@@ -1,17 +1,33 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { HeaderComponent } from './header.component';
+import { AuthUserService } from 'src/app/core/services/auth-user.service';
+import { MatIconModule } from '@angular/material/icon';
+
+class MockAuthUserService {
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  logOut() {}
+}
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
   let fixture: ComponentFixture<HeaderComponent>;
+  let mockAuthUserService: MockAuthUserService;
 
   beforeEach(async () => {
+    mockAuthUserService = jasmine.createSpyObj(['logOut']);
     await TestBed.configureTestingModule({
-      declarations: [ HeaderComponent ]
-    })
-    .compileComponents();
+      declarations: [HeaderComponent],
+      imports: [MatIconModule],
+      providers: [
+        {
+          provide: AuthUserService,
+          useValue: mockAuthUserService,
+        },
+      ],
+    }).compileComponents();
+  });
 
+  beforeEach(() => {
     fixture = TestBed.createComponent(HeaderComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -19,5 +35,10 @@ describe('HeaderComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should call logout', () => {
+    component.logout();
+    expect(mockAuthUserService.logOut).toHaveBeenCalled();
   });
 });
